0422a0adaf2f4c27a81c170be2a37ceee71dff94
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2022-09-06 20:18:31 -0300

    Chore: enhancement form

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,24 +1,35 @@
 import { Link } from "react-router-dom";
-import { Grid, Box, Typography, FormControl, Select, MenuItem, TextField, Button, Stack } from '@mui/material';
+import { Grid, Box, Typography, FormControl, Select, MenuItem, TextField, Button, Stack, InputLabel } from '@mui/material';
 import DogTitulo from '../assets/dogTitulo.svg';
-import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { useDebounce } from '../utils/useDebounce';
+import dayjs from "dayjs";
 
 function Home() {
-  const [porte, setPorte] = useState();
-  const [data, setData] = useState(); //debouce set data
+  const [porte, setPorte] = useState("");
+  const [nome, setNome] = useState("");
+  const [data, setData] = useState(dayjs());
+  const debouncedData = useDebounce(data, 5000);
+
+  const onSubmit = (data) => console.log(data);
+
+  const handleNome = (value) => {
+    console.log(value.target.value);
+    setNome(value);
+  }
 
   const handleChange = () => {
     console.log("OnChange");
   }
   const handleData = (newValue) => {
-    console.log(newValue.utc());
-    setData(newValue);
+    if (newValue !== undefined) {
+      setData(newValue);
+    }
   };
-
+  // /^[a-zA-Z ]{2,30}$/
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Grid container padding={0} sx={{
@@ -27,7 +38,7 @@ function Home() {
         paddingBottom: '1%',
         justifyContent: 'center'
       }}>
-        <Grid item sm={12} md={12} lg={12} sx={{
+        <Grid item xs={12} sm={12} md={12} lg={12} sx={{
           backgroundColor: '#4eff27',
           paddingTop: '10px',
           paddingBottom: '10px'
@@ -48,46 +59,58 @@ function Home() {
         }}>
           <Box sx={{
             backgroundColor: '#d3f2d3',
-            paddingTop: '10px',
-            paddingBottom: '20px',
-            borderRadius: '10%'
+            paddingTop: '20px',
+            paddingBottom: '25px',
+            borderRadius: '10%',
+            paddingRight: '15%',
+            paddingLeft: '15%'
           }}>
             <Typography variant='h5' fontWeight={400} sx={{ marginBottom: '1.5rem' }}>Agende seu horário</Typography>
-            <FormControl>
-              <Stack spacing={3}>
-                <TextField type='text' fullWidth label="Nome completo" />
+            <Stack spacing={3}>
+              <FormControl>
+                <TextField type='text' label="Nome completo" value={nome} onChange={handleNome} />
+              </FormControl>
+              <FormControl>
                 <DateTimePicker
                   label="Data e hora"
                   value={data}
                   onChange={handleData}
                   renderInput={(params) => <TextField {...params} />}
                 />
+              </FormControl>
+              <FormControl>
+                <InputLabel id='porte'>Porte</InputLabel>
                 <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
+                  labelId="porte"
+                  id="selectPorte"
                   value={porte}
                   label="Porte"
                   onChange={handleChange}
+                  sx={{ textAlign: 'left' }}
                 >
                   <MenuItem value={1}>Pequeno</MenuItem>
                   <MenuItem value={2}>Médio</MenuItem>
                   <MenuItem value={3}>Grande</MenuItem>
                 </Select>
+              </FormControl>
+              <FormControl >
+                <InputLabel id='servico'>Serviço</InputLabel>
                 <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
+                  labelId="servico"
+                  id="selectServico"
                   value={porte}
-                  label="Porte"
+                  label="Serviço"
                   onChange={handleChange}
+                  sx={{ textAlign: 'left' }}
                 >
                   <MenuItem value={1}>Banho e tosa</MenuItem>
                   <MenuItem value={2}>Consulta veterinária</MenuItem>
                   <MenuItem value={3}>Exames de laboratório</MenuItem>
                   <MenuItem value={4}>Hospedagem</MenuItem>
                 </Select>
-                <Button color="primary">Enviar</Button>
-              </Stack>
-            </FormControl>
+                <Button color="primary" onClick={onSubmit}>Enviar</Button>
+              </FormControl>
+            </Stack>
           </Box>
         </Grid>
       </Grid >
