c28f425a43defb5914d777ef7a1dd2cd1db00c70
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2022-09-08 23:34:10 -0300

    Improvment: form

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
-import { Grid, Box, Typography, FormControl, Select, MenuItem, TextField, Button, Stack, InputLabel } from '@mui/material';
+import { Grid, Box, Typography, FormControl, Select, MenuItem, TextField, Button, Stack, InputLabel, InputAdornment, FormControlLabel, Checkbox, FormGroup } from '@mui/material';
 import DogTitulo from '../assets/dogTitulo.svg';
+import PersonIcon from '@mui/icons-material/Person';
+import PetsIcon from '@mui/icons-material/Pets';
+import RoomServiceIcon from '@mui/icons-material/RoomService';
 import { useState } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { useDebounce } from '../utils/useDebounce';
+import { encontraPorte, encontraServico } from "../utils/enum/selectEnum";
 import dayjs from "dayjs";
 
 function Home() {
-  const [porte, setPorte] = useState("");
+  const [porte, setPorte] = useState('');
+  const [servico, setServico] = useState('');
   const [nome, setNome] = useState("");
   const [data, setData] = useState(dayjs());
   const debouncedData = useDebounce(data, 5000);
@@ -20,14 +25,22 @@ function Home() {
     setNome(value);
   }
 
-  const handleChange = () => {
-    console.log("OnChange");
+  const handlePorte = (e) => {
+    setPorte(e.target.value);
+  }
+
+  const handleServico = (e) => {
+    setServico(e.target.value);
   }
   const handleData = (newValue) => {
     if (newValue !== undefined) {
       setData(newValue);
     }
   };
+
+  const handleDisableCheckbox = () => {
+   return true;
+  }
   // /^[a-zA-Z ]{2,30}$/
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -42,7 +55,9 @@ function Home() {
           paddingTop: '10px',
           paddingBottom: '10px'
         }}>
-          <Typography fontWeight={600} variant="h4" >
+          <Typography fontWeight={600} variant="h4" sx={{
+            letterSpacing: '5px'
+          }} >
             <Box src={DogTitulo} component='img'
               sx={{
                 maxWidth: '45px',
@@ -53,21 +68,30 @@ function Home() {
           </Typography>
         </Grid>
         <Grid item sm={12} md={7} lg={7} sx={{
-          marginTop: '2rem',
+          marginTop: '8vh',
           textAlign: 'center',
         }}>
-          <Box sx={{
+          <Box 
+          sx={{
             backgroundColor: '#d3f2d3',
             paddingTop: '20px',
             paddingBottom: '25px',
             borderRadius: '10%',
             paddingRight: '15%',
-            paddingLeft: '15%'
+            paddingLeft: '15%',
+            boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'
           }}>
-            <Typography variant='h5' fontWeight={400} sx={{ marginBottom: '1.5rem' }}>Agende seu horário</Typography>
+            <Typography variant='h5' fontWeight={400} sx={{ marginBottom: '1.5rem', letterSpacing: '2px' }}>Agende seu horário</Typography>
             <Stack spacing={3}>
               <FormControl>
-                <TextField type='text' label="Nome completo" value={nome} onChange={(e) => handleNome(e.target.value)} />
+                <TextField type='text' label="Nome completo" value={nome} onChange={(e) => handleNome(e.target.value)}  
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <PersonIcon />
+                    </InputAdornment>
+                  ),
+                }} />
               </FormControl>
               <FormControl>
                 <DateTimePicker
@@ -78,37 +102,61 @@ function Home() {
                 />
               </FormControl>
               <FormControl>
-                <InputLabel id='porte'>Porte</InputLabel>
+                <InputLabel id="labelPorte">Porte</InputLabel>
                 <Select
-                  labelId="porte"
-                  id="selectPorte"
                   value={porte}
                   label="Porte"
-                  onChange={handleChange}
+                  labelId="labelPorte"
+                  onChange={handlePorte}
                   sx={{ textAlign: 'left' }}
+                  endAdornment={
+                    <InputAdornment position="end" sx={{
+                      marginRight: "1rem"
+                    }}>
+                      <PetsIcon />
+                    </InputAdornment>
+                  }
                 >
-                  <MenuItem value={1}>Pequeno</MenuItem>
-                  <MenuItem value={2}>Médio</MenuItem>
-                  <MenuItem value={3}>Grande</MenuItem>
+                  <MenuItem value={encontraPorte(1)}>{encontraPorte(1).nome}</MenuItem>
+                  <MenuItem value={encontraPorte(2)}>{encontraPorte(2).nome}</MenuItem>
+                  <MenuItem value={encontraPorte(3)}>{encontraPorte(3).nome}</MenuItem>
                 </Select>
               </FormControl>
               <FormControl >
-                <InputLabel id='servico'>Serviço</InputLabel>
+              <InputLabel id="labelServico">Serviço</InputLabel>
                 <Select
-                  labelId="servico"
-                  id="selectServico"
-                  value={porte}
+                  value={servico}
                   label="Serviço"
-                  onChange={handleChange}
+                  labelId="labelServico"
+                  onChange={handleServico}
                   sx={{ textAlign: 'left' }}
+                  endAdornment={
+                    <InputAdornment position="end" sx={{
+                      marginRight: "1rem"
+                    }}>
+                      <RoomServiceIcon />
+                    </InputAdornment>
+                  }
                 >
-                  <MenuItem value={1}>Banho e tosa</MenuItem>
-                  <MenuItem value={2}>Consulta veterinária</MenuItem>
-                  <MenuItem value={3}>Exames de laboratório</MenuItem>
-                  <MenuItem value={4}>Hospedagem</MenuItem>
+                  <MenuItem value={encontraServico(1)}>{encontraServico(1).nome}</MenuItem>
+                  <MenuItem value={encontraServico(2)}>{encontraServico(2).nome}</MenuItem>
+                  <MenuItem value={encontraServico(3)}>{encontraServico(3).nome}</MenuItem>
                 </Select>
-                <Button color="primary" onClick={onSubmit}>Enviar</Button>
               </FormControl>
+              <FormGroup sx={{
+                display: 'table-row-group'
+              }}>
+                <FormControlLabel control={<Checkbox disabled={true} />} label="Banho" />
+                <FormControlLabel control={<Checkbox />} label="Tosa" />
+                <FormControlLabel control={<Checkbox />} label="Desembaraçamento" />
+                <FormControlLabel control={<Checkbox />} label="Corte de unhas" />
+                <FormControlLabel control={<Checkbox />} label="Hidratação" />
+                <FormControlLabel control={<Checkbox />} label="Consulta" />
+                <FormControlLabel control={<Checkbox />} label="Vacinação" />
+                <FormControlLabel control={<Checkbox />} label="Passeio" />
+                <FormControlLabel control={<Checkbox />} label="Adestramento" />
+              </FormGroup>
+              <Button color="primary" onClick={onSubmit} sx={{marginTop: '15px', borderRadius: '10%', color: 'darkblue', backgroundColor: '#c4e3cd'}}>Enviar</Button>
             </Stack>
           </Box>
         </Grid>
