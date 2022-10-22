00c90919b5196996cdc1d50af6ffaf4e3a7885a9
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2022-10-22 01:33:59 -0300

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ],
+    'comma-dangle': [
+      'error',
+      'never'
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 90
+      }
+    ]
+  }
+};
