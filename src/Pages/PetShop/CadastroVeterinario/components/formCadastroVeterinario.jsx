7c850b2f6435f6842f7b181c34bd4d2f273ca0d1
Guilherme Souza Araújo <guilherme.souza@dtidigital.com.br>
2022-11-12 07:34:07 -0300

    chore: formCadastroVet

--- src/Pages/PetShop/CadastroVeterinario/components/formCadastroVeterinario.jsx
+++ src/Pages/PetShop/CadastroVeterinario/components/formCadastroVeterinario.jsx
@@ -1,6 +1,6 @@
+import { useState } from 'react';
 import {
 	Grid,
-	Button,
 	Avatar,
 	Typography,
 	useTheme,
@@ -11,8 +11,10 @@ import {
 	Stack,
 	InputAdornment,
 	MenuItem,
+	Tooltip,
 } from '@mui/material';
 import PetsIcon from '@mui/icons-material/Pets';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import AlertaErroForm from '../../../../shared/components/erroForm';
@@ -23,6 +25,8 @@ import CadastroVeterinarioService from '../service/cadastroVeterinarioService';
 
 export default function FormCadastroVeterinario() {
 	const theme = useTheme();
+	const [loading, setLoading] = useState(false);
+	const [toastIsOpen, setToastIsOpen] = useState(false);
 
 	const { data: users } = useQuery(
 		'vets',
@@ -38,6 +42,10 @@ export default function FormCadastroVeterinario() {
 		defaultValues: { user_id: '', crm: '', specialization: '' },
 		mode: 'onChange',
 	});
+
+	const onSubmit = async (data) => {
+		console.log(data);
+	};
 
 	return (
 		<Container component="main" maxWidth="xs" bc={theme.palette.primary.main}>
@@ -65,35 +73,40 @@ export default function FormCadastroVeterinario() {
 						backgroundColor: theme.palette.primary.light,
 						borderRadius: '3%',
 						border: '1px solid white',
+						maxWidth: '280px',
 					}}
 				>
 					<form id="cadastroPet">
 						<Stack spacing={2}>
 							<Grid item xs={12} sm={12} md={12} lg={12}>
-								{users && users.data && users.data.length > 0 ? (<Controller
-									name="user_id"
-									control={control}
-									rules={{
-										required: true,
-									}}
-									render={({ field: { onChange, value } }) => (
-										<TextField
-											select
-											fullWidth
-											label="Usuários cadastrados"
-											value={value}
-											onChange={onChange}
-											error={errors.user_id}
-											helperText={errors.user_id?.message}
-										>
-											{users.data.map((user) => (
-												<MenuItem key={user.id} value={user.id}>
-													{user.name}
-												</MenuItem>
-											))}
-										</TextField>
-									)}
-								/>) : (<></>)}
+								{users && users.data && users.data.length > 0 ? (
+									<Controller
+										name="user_id"
+										control={control}
+										rules={{
+											required: true,
+										}}
+										render={({ field: { onChange, value } }) => (
+											<TextField
+												select
+												fullWidth
+												label="Usuários cadastrados"
+												value={value}
+												onChange={onChange}
+												error={errors.user_id}
+												helperText={errors.user_id?.message}
+											>
+												{users.data.map((user) => (
+													<MenuItem key={user.id} value={user.id}>
+														{user.name}
+													</MenuItem>
+												))}
+											</TextField>
+										)}
+									/>
+								) : (
+									<></>
+								)}
 								{errors.user_id && <AlertaErroForm textoErro="Campo obrigatório" />}
 							</Grid>
 							<Grid item xs={12} sm={12} md={12} lg={12}>
@@ -102,23 +115,27 @@ export default function FormCadastroVeterinario() {
 									control={control}
 									rules={{
 										required: true,
+										maxLength: 8,
+										minLength: 8,
 									}}
 									render={({ field: { onChange, value } }) => (
-										<TextField
-											fullWidth
-											label="CRMV"
-											value={value}
-											onChange={onChange}
-											error={errors.size}
-											helperText={errors.size?.message}
-											InputProps={{
-												endAdornment: (
-													<InputAdornment position="start">
-														<HealthAndSafetyIcon />
-													</InputAdornment>
-												),
-											}}
-										/>
+										<Tooltip title="O padrão de CRMV é: UF-12345" placement='right'>
+											<TextField
+												fullWidth
+												label="CRMV"
+												value={value}
+												onChange={onChange}
+												error={errors.size}
+												helperText={errors.size?.message}
+												InputProps={{
+													endAdornment: (
+														<InputAdornment position="start">
+															<HealthAndSafetyIcon />
+														</InputAdornment>
+													),
+												}}
+											/>
+										</Tooltip>
 									)}
 								/>
 								{errors.crm && <AlertaErroForm textoErro="Campo obrigatório" />}
@@ -153,7 +170,18 @@ export default function FormCadastroVeterinario() {
 								)}
 							</Grid>
 							<Grid item xs={12} sm={12} md={12} lg={12} textAlign="center">
-								<StyledLoadingButton>Cadastrar</StyledLoadingButton>
+								<StyledLoadingButton
+									onClick={handleSubmit(onSubmit)}
+									loading={loading}
+									loadingPosition="end"
+									disabled={!isDirty || !isValid}
+									fullWidth
+									variant="contained"
+									sx={{ mt: 1, color: theme.palette.primary.dark }}
+									endIcon={<AddCircleIcon />}
+								>
+									Cadastrar
+								</StyledLoadingButton>
 							</Grid>
 						</Stack>
 					</form>
